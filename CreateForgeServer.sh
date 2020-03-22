ff9b129b28fe0eee92e0fd5f342c6935f37275d3
CrypticSafe5 <WesleyBrackett94@gmail.com>
2020-03-22 12:00:35 -0500

    Fixed forge version donwnload

--- CreateForgeServer.sh
+++ CreateForgeServer.sh
@@ -64,7 +64,7 @@ do
 done
 
 # Setup
-URL_FORGE_INSTALLER='https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12-14.21.1.2387/forge-1.12-14.21.1.2387-installer.jar'
+URL_FORGE_INSTALLER='https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.12.2-14.23.5.2768/forge-1.12.2-14.23.5.2768-installer.jar'
 URL_MODPACK='https://media.forgecdn.net/files/2836/137/RLCraft+1.12.2+-+Beta+v2.8.1.zip'
 OLD_IFS=${IFS}
 DIR="$(pwd)/${OUTPUT_DIR}"
